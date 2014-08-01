aecf2ddf33e5b66158ea00a1763791ac4c2716c2
Raphaël <r.charbey@gmail.com>
2014-08-01 17:33:06 +0200

    ?

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append("../Methodes_graphe")
 import methods_graph
 import enumerate
+import enumerate_mobystar
 
 parser = argparse.ArgumentParser(description="main")
 parser.add_argument('k', help="taille maximale des patterns a enumerer")
@@ -11,8 +12,12 @@ parser.add_argument('nom', help="nom de ego")
 parser.add_argument('--option', '-o', nargs='+')
 args = parser.parse_args()
 
-triple = methods_graph.create_graph(args.dossier, args.nom)
-graph = triple[0]
+if args.option == None :
+    triple = methods_graph.create_graph(args.dossier, args.nom)
+    graph = triple[0]
+else:
+    graph = enumerate_mobystar.create_graph_tel()
 print "|N| = "+str(len(graph.vs)) +",  |E| = "+str(len(graph.es)) 
 couple = enumerate.characterize_with_patterns(graph, int(args.k))
 print couple[0]
+
